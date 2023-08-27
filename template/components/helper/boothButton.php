720526d7248bd20f2eb2e0689ace587c42f23757
ledsi <rrsnooop@gmail.com>
2023-08-27 12:44:13 +0200

    Add new EVOLUTION theme
    Changes by Andreas Blaesius <andi@unlegacy-android.org>
    - fix standalone gallery
    - add error handling for gallery images,
    - use i18n on empty gallery
    - fix rotary controller and CUPS button
    - use unified result button template
    - use unified logo component
    
    Change-Id: Ib4bd97ea3701c911a4fecc4cd8e98a009a29e481

--- template/components/helper/boothButton.php
+++ template/components/helper/boothButton.php
@@ -1,7 +1,16 @@
 <?php
 function getBoothButton($label, $icon, $command, $type = 'md') {
     global $config;
-    $btnClass = 'btn btn--' . $config['ui']['button'];
+    if ($config['ui']['style'] == 'evolution') {
+        $btnClass = "
+            w-24 h-24 sm:w-28 sm:h-28 m-2 p-2 flex text-white text-4xl bg-black bg-opacity-50 hover:bg-opacity-75 backdrop-filter backdrop-blur-lg
+            border border-solid border-white border-opacity-70 rounded-3xl shadow-lg
+            flex items-center justify-center flex-col shrink-0 grow-0 outline-none focus:outline-none active:outline-none
+            hover:bg-white hover:text-black hover:bg-opacity-100 transition-all duration-200 ease-in-out
+        ";
+    } else {
+        $btnClass = 'btn btn--' . $config['ui']['button'];
+    }
     $btnClass .= ' ' . $command;
 
     if (($command != 'deletebtn' && $label != 'cups-button') || ($command == 'deletebtn' && $config['delete']['no_request'])) {
@@ -17,11 +26,14 @@ function getBoothButton($label, $icon, $command, $type = 'md') {
             ' !w-full !h-full absolute rounded-none border-opacity-30 border-dashed m-0 !mb-2 p-0 flex items-center justify-center flex-col !bg-transparent hover:text-white';
     }
 
-    if (($config['ui']['style'] == 'classic' || $config['ui']['style'] == 'classic_rounded') && $type == 'xs') {
-        $btnClass .= ' btn--small';
+    if ($config['ui']['style'] == 'evolution' && $type == 'xs') {
+        $btnClass .= ' !w-20 !h-20 !m-1 !p-1 !text-2xl !rounded-xl';
     }
 
     if ($label == 'cups-button') {
+        if ($config['ui']['style'] == 'evolution') {
+            $btnClass .= ' !absolute !top-4 !left-4';
+        }
         $btnClass .= '" target="newwin';
     }
 
